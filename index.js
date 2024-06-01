018cf79888570dcba9e269678d9843860821f253
TriBhaskar <panthribhaskar@gmail.com>
2024-06-01 22:10:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const port = 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
